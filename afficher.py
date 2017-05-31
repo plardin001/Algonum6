108cb48e305690b3f4c820e0fa160385dfce58a1
plardin001 <pierre-git@oustalade.net>
2017-05-31 18:05:16 +0200

    test dim2

--- afficher.py
+++ afficher.py
@@ -4,7 +4,7 @@ from method import *
 
 #print a vector field for the cauchy problem : y'(t)= f(y(t),t) ( f : R2 -> R) )
 #with y(t0) = y0 as a initial condition
-def print_field(f, y0, t0, field_name,method, h=0.01, xmin=0, xmax=10, ymin=0, ymax=10):
+def hs_print_field(f, y0, t0, field_name,method, h=0.01, xmin=0, xmax=10, ymin=0, ymax=10):
     X = np.arange(xmin, xmax, h)
     Y = np.arange(ymin, ymax, h) #maillage
     N = len(X)
@@ -31,3 +31,18 @@ def print_field(f, y0, t0, field_name,method, h=0.01, xmin=0, xmax=10, ymin=0, y
                DF[::step])
     plt.show()
     return
+"""
+
+def print_field(f, y0, t0, field_name,method, h=0.01, xmin=0, xmax=10, ymin=0, ymax=10):
+    X = np.arange(xmin, xmax, h)
+    Y = np.arange(ymin, ymax, h) #maillage
+    N = len(X)
+    dX,dY=VdPol([X1 ,Y1],0) # generer  les  vecteurs  tangents
+    M=hypot(dX,dY)# normalisation
+    M[M==0]=1.
+    dX /= M # remplaces  par 1 avant  division
+    dY /= M
+    quiver(Y1 ,X1 ,dY ,dX,M)
+    # generation du  champs  de vecteurs
+    show ()
+"""
